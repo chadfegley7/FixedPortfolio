35bc8b3e7245534c63ff19a35a0cf38f83d5b779
Chad Fegley <chadfegley7@gmail.com>
2016-05-04 17:08:42 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express= require("express");
+var path= require("path");
+var bodyParser = require("body-parser");
+var app = express();
+require("./server/config/mongoose.js");
+
+app.use(express.static(path.join(__dirname, "./client")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var mongoose = require("mongoose");
+
+require("./server/config/routes.js");
+
+app.listen(8000, function(){
+  console.log("Plan a trip on port 8000!!!")
+});
