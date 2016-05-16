743dd0de60a4f603d5260adf45e3251e6862340e
Chad Fegley <chadfegley7@gmail.com>
2016-05-16 10:12:21 -0700

    added more functionality

--- client/js/model/picFactory.js
+++ client/js/model/picFactory.js
@@ -9,11 +9,11 @@ myApp.factory('picFactory', function($http){
     })
   }
   factory.jsonFlickrApi = function(callback){
-    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=81a3ca2a73f2dcc166abed21c269dcc8&user_id=134646885%40N03&format=json&nojsoncallback=1&auth_token=72157667654837380-4028b6dd8df1cb0e&api_sig=d2689c5e8cf46144ef3ccb8f6fb8bd24").success(function(output){
+    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=202a9ce136a30fdd03a911afc5157310&user_id=134646885%40N03&format=json&nojsoncallback=1&auth_token=72157668162025372-26a48b640630bc93&api_sig=b6ca42d405d23bf1eb4c98849db81768").success(function(output){
       callback(output);
       console.log(output);
       gallery = output.photos.photo;
-      
+
     })
   }
   return factory;
