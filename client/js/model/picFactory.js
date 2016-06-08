9016ce7f890375df247cac3aa3c5bdc25627a9e4
Chad Fegley <chadfegley7@gmail.com>
2016-06-08 11:07:19 -0700

    fixing minor errors

--- client/js/model/picFactory.js
+++ client/js/model/picFactory.js
@@ -9,7 +9,7 @@ myApp.factory('picFactory', function($http){
     })
   }
   factory.jsonFlickrApi = function(callback){
-    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=4dfa0e3ce9920a9e3f3af12a6883c68a&user_id=134646885%40N03&per_page=50&format=json&auth_token=72157669502165985-a321593ace321009&api_sig=7c2ab5c760407b59c72e47bc8e160d7e").success(function(output){
+    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=4dfa0e3ce9920a9e3f3af12a6883c68a&user_id=134646885%40N03&per_page=50&format=json&nojsoncallback=1&auth_token=72157669502165985-a321593ace321009&api_sig=c5047489cc93c15fd129d10db97d0fb8").success(function(output){
       callback(output);
       console.log(output);
       gallery = output.photos.photo;
