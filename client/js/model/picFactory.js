03cf67556635a161e41f890f826b18050756aeb8
Chad Fegley <chadfegley7@gmail.com>
2016-06-15 10:33:32 -0700

    added hover functionality and greyscale

--- client/js/model/picFactory.js
+++ client/js/model/picFactory.js
@@ -9,7 +9,7 @@ myApp.factory('picFactory', function($http){
     })
   }
   factory.jsonFlickrApi = function(callback){
-    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=f9be4bc1efe094acec24d58de12677d9&user_id=134646885%40N03&per_page=50&format=json&nojsoncallback=1&auth_token=72157669240898832-b27b278a5038a740&api_sig=c03b2dfa2c131a7982bbaba8f14b2b3d").success(function(output){
+    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=a8241be535e0dcf5c8788b736e5e6f91&user_id=134646885%40N03&per_page=50&format=json&nojsoncallback=1&auth_token=72157669803438535-7a765dbf804cbaa4&api_sig=30a14df2c2278fb47f5e72f306c6cb4f").success(function(output){
       callback(output);
       console.log(output);
       gallery = output.photos.photo;
