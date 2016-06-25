5dbd2f2fe23de3f357c588ed5a7cc1277215f51c
Chad Fegley <chadfegley7@gmail.com>
2016-06-25 11:07:30 -0700

    changing the layout

--- client/js/model/picFactory.js
+++ client/js/model/picFactory.js
@@ -9,7 +9,7 @@ myApp.factory('picFactory', function($http){
     })
   }
   factory.jsonFlickrApi = function(callback){
-    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=91c8572d01c41af920945c0ccd9cd2e5&user_id=134646885%40N03&per_page=100&format=json&nojsoncallback=1&auth_token=72157668951994610-860282660c4eb0c3&api_sig=3616f8b643fd00b92b875f2d681cc918").success(function(output){
+    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=a87c295ff848833c0b7ef6cbe1a12f89&user_id=134646885%40N03&per_page=100&format=json&nojsoncallback=1&auth_token=72157667482853544-323f2bb53d95f6f4&api_sig=fcccf8ea8c922f139c36bb4dd7d81e0f").success(function(output){
       callback(output);
       console.log(output);
       gallery = output.photos.photo;
