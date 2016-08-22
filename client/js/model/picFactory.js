8f460d88250eac4a46a18f841452f0ed8ba08bac
Chad Fegley <chadfegley7@gmail.com>
2016-08-22 01:19:29 -0700

    daily updates

--- client/js/model/picFactory.js
+++ client/js/model/picFactory.js
@@ -9,7 +9,7 @@ myApp.factory('picFactory', function($http){
     })
   }
   factory.jsonFlickrApi = function(callback){
-    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=8b93636d24f88ec1235f57579b38e851&user_id=134646885%40N03&per_page=25&format=json&nojsoncallback=1&auth_token=72157672674782836-97cb8d0d83510309&api_sig=264f9d626c7ae1421754af2d3548c00a").success(function(output){
+    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=3f9fe783e105a01013fbe2d4de25ce17&user_id=134646885%40N03&per_page=25&format=json&nojsoncallback=1&auth_token=72157672703228346-6ec2a13dea7b12ec&api_sig=1aa0e638ef1eaefb0e81370e879a1e85").success(function(output){
       callback(output);
       console.log(output);
       gallery = output.photos.photo;
