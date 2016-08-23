30eee08d4a120e9286ef9ef3d2a4988478dcc0f3
Chad Fegley <chadfegley7@gmail.com>
2016-08-22 18:07:58 -0700

    updates on the layout

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express= require("express");
 var path= require("path");
 var bodyParser = require("body-parser");
+var gumerPSN = require("gumer-psn");
 var app = express();
 require("./server/config/mongoose.js");
 
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 var mongoose = require("mongoose");
 
 require("./server/config/routes.js");
+
+
 
 app.listen(8000, function(){
   console.log("Plan a trip on port 8000!!!")
