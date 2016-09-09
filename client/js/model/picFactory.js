a9b69d764024bfb119d72c0c7241920e51334e6d
Chad Fegley <chadfegley7@gmail.com>
2016-09-08 18:04:21 -0700

    added React Weather App

--- client/js/model/picFactory.js
+++ client/js/model/picFactory.js
@@ -9,7 +9,7 @@ myApp.factory('picFactory', function($http){
     })
   }
   factory.jsonFlickrApi = function(callback){
-    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=ac2c8d3f551c319eac4ae5d47b422c0f&user_id=134646885%40N03&per_page=15&format=json&nojsoncallback=1&auth_token=72157672516426531-fbae44204425d433&api_sig=a68d9bad6af185a9225dc8e80788e03a").success(function(output){
+    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=e10126458579a7d546cfda49dd368a42&user_id=134646885%40N03&per_page=15&format=json&nojsoncallback=1&auth_token=72157673609361445-e2222ae4cdcc95d7&api_sig=648e2e9aff302b2b40ec728fb59823e6").success(function(output){
       callback(output);
       console.log(output);
       gallery = output.photos.photo;
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express= require("express");
 var path= require("path");
 var bodyParser = require("body-parser");
-var gumerPSN = require("gumer-psn");
 var app = express();
-require("./server/config/mongoose.js");
+
 
 app.use(express.static(path.join(__dirname, "./client")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var mongoose = require("mongoose");
+
 
 require("./server/config/routes.js");
 
