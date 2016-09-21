d5419cd882d86e52f6b8e31b26623acc04a200fb
Chad Fegley <chadfegley7@gmail.com>
2016-09-21 01:16:13 -0700

    hopefully this works

--- client/js/model/picFactory.js
+++ client/js/model/picFactory.js
@@ -9,7 +9,7 @@ myApp.factory('picFactory', function($http){
     })
   }
   factory.jsonFlickrApi = function(callback){
-    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=e10126458579a7d546cfda49dd368a42&user_id=134646885%40N03&per_page=15&format=json&nojsoncallback=1&auth_token=72157673609361445-e2222ae4cdcc95d7&api_sig=648e2e9aff302b2b40ec728fb59823e6").success(function(output){
+    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=a4e3ada256502e1d55b15c9bc91e4402&user_id=134646885%40N03&per_page=15&format=json&nojsoncallback=1&auth_token=72157674077421076-3a6cd24e80d2bc66&api_sig=b64320d39cbd3e33598a02ebc97b91ae").success(function(output){
       callback(output);
       console.log(output);
       gallery = output.photos.photo;
