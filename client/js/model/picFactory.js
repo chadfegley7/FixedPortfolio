66e3b256ac192eb5e88e58e790856e94c921e631
Chad Fegley <chadfegley7@gmail.com>
2016-09-21 14:00:08 -0700

    added highflyers app

--- client/js/model/picFactory.js
+++ client/js/model/picFactory.js
@@ -9,7 +9,7 @@ myApp.factory('picFactory', function($http){
     })
   }
   factory.jsonFlickrApi = function(callback){
-    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=a4e3ada256502e1d55b15c9bc91e4402&user_id=134646885%40N03&per_page=15&format=json&nojsoncallback=1&auth_token=72157674077421076-3a6cd24e80d2bc66&api_sig=b64320d39cbd3e33598a02ebc97b91ae").success(function(output){
+    $http.get("https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=ffd9cc8b42047e8713d07cbc8345b49f&user_id=134646885%40N03&per_page=15&format=json&nojsoncallback=1&auth_token=72157674199781725-305c99264b673f05&api_sig=cb79eb0eabfb4f7d243f18e30ad5542b").success(function(output){
       callback(output);
       console.log(output);
       gallery = output.photos.photo;
